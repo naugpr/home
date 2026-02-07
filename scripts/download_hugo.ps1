974ac8193131c4cb3dde2b3e1247aca9ff73b162
gismanray <57723407+gismanray@users.noreply.github.com>
2026-02-07 11:16:52 -0700

    Home repository updated 2026-02-07 11:16 a.m.

--- scripts/download_hugo.ps1
+++ scripts/download_hugo.ps1
@@ -1,4 +1,4 @@
-$vendor = 'D:\github\naugpr\home\start\.pnpm\hugo-bin@0.114.2\node_modules\hugo-bin\vendor'
+$vendor = 'D:\Hugoblox\home\.pnpm\hugo-bin@0.114.2\node_modules\hugo-bin\vendor'
 New-Item -ItemType Directory -Force -Path $vendor | Out-Null
 $zip = Join-Path $env:TEMP 'hugo_0.118.2.zip'
 Invoke-WebRequest -Uri 'https://github.com/gohugoio/hugo/releases/download/v0.118.2/hugo_0.118.2_windows-amd64.zip' -OutFile $zip -UseBasicParsing
